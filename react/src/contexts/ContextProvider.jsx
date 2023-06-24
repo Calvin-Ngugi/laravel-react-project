fded0865e32bb77e64cfb88f1a9fab3cbdcaf815
Calvin Ngugi <ncalvin67@gmail.com>
2023-06-24 23:36:58 +0300

    :art: :heavy_plus_sign: Adding axios to signup

--- react/src/contexts/ContextProvider.jsx
+++ react/src/contexts/ContextProvider.jsx
@@ -3,34 +3,36 @@ import { createContext, useContext, useState } from "react";
 const StateContext = createContext({
     currentUser: null,
     token: null,
-    setUser: () => { },
-    setToken: () => { }
-})
+    setUser: () => {},
+    setToken: () => {},
+});
 
 // eslint-disable-next-line react/prop-types
-export const ContextProvider = ({children}) => {
+export const ContextProvider = ({ children }) => {
     const [user, setUser] = useState({});
-    const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN'));
+    const [token, _setToken] = useState(localStorage.getItem("ACCESS_TOKEN"));
 
     const setToken = (token) => {
-        _setToken(token)
+        _setToken(token);
         if (token) {
-            localStorage.setItem('ACCESS_TOKEN', token);
+            localStorage.setItem("ACCESS_TOKEN", token);
         } else {
-            localStorage.removeItem('ACCESS_TOKEN')
+            localStorage.removeItem("ACCESS_TOKEN");
         }
-    }
+    };
 
     return (
-        <StateContext.Provider value={{
-            user,
-            token,
-            setUser,
-            setToken
-        }}>
+        <StateContext.Provider
+            value={{
+                user,
+                token,
+                setUser,
+                setToken,
+            }}
+        >
             {children}
         </StateContext.Provider>
-    )
-}
+    );
+};
 
-export const useStateContext = () => useContext(StateContext)
+export const useStateContext = () => useContext(StateContext);
