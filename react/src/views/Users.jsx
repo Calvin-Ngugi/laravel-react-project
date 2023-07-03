f8c05c437d50baf6c329288b4d078c7870ab4c78
Calvin Ngugi <ncalvin67@gmail.com>
2023-07-03 07:39:00 +0300

    Minor changes

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,7 +1,24 @@
-const Users = () => {
-  return (
-    <div>Users</div>
-  )
-}
+import { useEffect, useState } from "react";
+import axiosClient from "../axios-client";
 
-export default Users
+const Users = () => {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {}, []);
+
+    const getUsers = () => {
+        setLoading(true);
+        axiosClient.get("/users")
+            .then(({ data }) => {
+                setLoading(false);
+                console.log(data);
+            })
+            .catch(() => {
+                setLoading(false);
+            });
+    };
+    return <div>Users</div>;
+};
+
+export default Users;
